8a6d0733f520dfc68caa83a5c21d38424eca0e06
brunodacunha <brunoc.dacunha@hotmail.com>
2015-06-16 20:39:27 -0300

    Menu changes

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
   get "sign_up" => "users#new", :as => "sign_up"
-  root :to => "sessions#new"
+  #root :to => "sessions#new"
   resources :users
   resources :sessions
   #added by bruno END
@@ -35,7 +35,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'home#index'
-  #root :to => 'home#index'
+  root :to => 'sessions#new'
 
 # Example of regular route:
 #   get 'products/:id' => 'catalog#view'
